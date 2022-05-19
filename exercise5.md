f523409ecc348da419578781743cfb74b8c00bac
AbdullahKaitoua <abdullah.kaitoua@edu.fh-joanneum.at>
2022-05-19 10:38:19 +0200

    ex5_2

---
+++ exercise5.md
@@ -0,0 +1,18 @@
+# Exercise 5
+-create a new branch logging via git branch 
+-Switch to the logging branch via git checkout
+
+##ex5-1
+![Screenshot](resources/images/ex5_1.png)
+
+##ex5.2
+
+-Create Maven project with Log4j2 dependency
+-Obtain a Logger
+-Use INFO and ERROR levels to log 
+-by default Log4j assigns the root logger to Level.ERROR and those logs will be printed on the standard console. 
+-ALL < TRACE < DEBUG < INFO < WARN < ERROR < FATAL
+So since the default level is set to ERROR, error  will be logged.
+
+![Screenshot](resources/images/ex5_2.png)
+
--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,11 +1,20 @@
 package at.fhj.msd;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static Logger logger = LogManager.getLogger();
     public static void main(String[] args) {
         Calculator calc =new Calculator();
+
         System.out.println(calc.add(29, 2));
         System.out.println(calc.minus(22, 2));
         System.out.println(calc.divide(26, 5));
         System.out.println(calc.multiply(10, 30));
+        logger.info("It is a info logger.");
+        logger.error("It is an error logger.");
+
+
+
     }
 }
